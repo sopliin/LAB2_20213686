fc706ed892a481c87c6347b7151eb5c35aa082fc
rodrigo <rodrigo@example.com>
2026-04-16 20:23:45 -0500

    creacion rama develop

---
+++ src/test/java/com/example/lab2gticsb/Lab2gticsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2gticsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2gticsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
